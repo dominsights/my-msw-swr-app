1a499ca4a1d36445104184a5850c036848f831b5
Domício Medeiros <domicio.medeiros@proton.me>
2024-10-06 15:44:36 -0300

    ...

--- src/components/SubscriberListItem.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Subscriber } from '../lib/models/subscriber'
-
-export default function SubscriberListItem({subscriber} : {subscriber: Subscriber}) {
-  return (
-    <li>{subscriber.name} {subscriber.age}</li>
-  )
-}
