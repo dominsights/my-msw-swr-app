56cd0ab57411f1c6b5199ded1393db27c074e871
Domício Medeiros <domicio.medeiros@proton.me>
2024-10-06 17:26:38 -0300

    add dialog form

--- src/components/Subscribers.tsx
+++ src/components/Subscribers.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import useSubscribers from "./useSubscribers";
-import { Button, Table } from "@radix-ui/themes";
+import { Button, Dialog, Flex, Table, Text, TextField } from "@radix-ui/themes";
 import { PlusIcon } from "@radix-ui/react-icons";
 
 export default function Demo() {
@@ -9,29 +9,66 @@ export default function Demo() {
 
   return (
     <>
-      <Button>
-        <PlusIcon /> New subscriber
-      </Button>
       {data ? (
-        <Table.Root>
-          <Table.Header>
-            <Table.Row>
-              <Table.ColumnHeaderCell>Full name</Table.ColumnHeaderCell>
-              <Table.ColumnHeaderCell>Email</Table.ColumnHeaderCell>
-              <Table.ColumnHeaderCell>Status</Table.ColumnHeaderCell>
-            </Table.Row>
-          </Table.Header>
+        <>
+          <Dialog.Root>
+            <Dialog.Trigger>
+              <Button>
+                <PlusIcon /> New subscriber
+              </Button>
+            </Dialog.Trigger>
+            <Dialog.Content>
+              <Dialog.Title>Add new subscriber</Dialog.Title>
+              <Dialog.Description size="2" mb="4">
+                Enter the new subscriber information.
+              </Dialog.Description>
+              <Flex direction="column" gap="3">
+                <label>
+                  <Text as="div" size="2" mb="1" weight="bold">
+                    Full name
+                  </Text>
+                  <TextField.Root placeholder="Enter the subscribers's full name" />
+                </label>
+                <label>
+                  <Text as="div" size="2" mb="1" weight="bold">
+                    Email
+                  </Text>
+                  <TextField.Root placeholder="Enter the subscriber's email" />
+                </label>
+              </Flex>
 
-          <Table.Body>
-            {data?.map((s, i) => (
-              <Table.Row key={i}>
-                <Table.RowHeaderCell>{s.fullName}</Table.RowHeaderCell>
-                <Table.Cell>{s.email}</Table.Cell>
-                <Table.Cell>{s.status ? "Active" : "Inactive"}</Table.Cell>
+              <Flex gap="3" mt="4" justify="end">
+                <Dialog.Close>
+                  <Button variant="soft" color="gray">
+                    Cancel
+                  </Button>
+                </Dialog.Close>
+                <Dialog.Close>
+                  <Button>Save</Button>
+                </Dialog.Close>
+              </Flex>
+            </Dialog.Content>
+          </Dialog.Root>
+          <Table.Root>
+            <Table.Header>
+              <Table.Row>
+                <Table.ColumnHeaderCell>Full name</Table.ColumnHeaderCell>
+                <Table.ColumnHeaderCell>Email</Table.ColumnHeaderCell>
+                <Table.ColumnHeaderCell>Status</Table.ColumnHeaderCell>
               </Table.Row>
-            ))}
-          </Table.Body>
-        </Table.Root>
+            </Table.Header>
+
+            <Table.Body>
+              {data?.map((s, i) => (
+                <Table.Row key={i}>
+                  <Table.RowHeaderCell>{s.fullName}</Table.RowHeaderCell>
+                  <Table.Cell>{s.email}</Table.Cell>
+                  <Table.Cell>{s.status ? "Active" : "Inactive"}</Table.Cell>
+                </Table.Row>
+              ))}
+            </Table.Body>
+          </Table.Root>
+        </>
       ) : (
         <div>Loading</div>
       )}
