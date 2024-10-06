8598825fb56362e47c16d3b6aedd5ab54bf776fd
Domício Medeiros <domicio.medeiros@proton.me>
2024-10-06 15:06:44 -0300

    ...

--- src/components/Subscribers.tsx
+++ src/components/Subscribers.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React from "react";
 import useSubscribers from "./useSubscribers";
-import SubscriberListItem from "./SubscriberListItem";
 import { Button, Table } from "@radix-ui/themes";
 import { PlusIcon } from "@radix-ui/react-icons";
 
@@ -10,30 +9,29 @@ export default function Demo() {
 
   return (
     <>
-    <Button>
-      <PlusIcon/> New subscriber
-    </Button>
+      <Button>
+        <PlusIcon /> New subscriber
+      </Button>
       {data ? (
-            <Table.Root>
-            <Table.Header>
-              <Table.Row>
-                <Table.ColumnHeaderCell>Full name</Table.ColumnHeaderCell>
-                <Table.ColumnHeaderCell>Email</Table.ColumnHeaderCell>
-                <Table.ColumnHeaderCell>Status</Table.ColumnHeaderCell>
-              </Table.Row>
-            </Table.Header>
-          
-            <Table.Body>
-              {data?.map((s, i) => 
-                <Table.Row key={i}>
+        <Table.Root>
+          <Table.Header>
+            <Table.Row>
+              <Table.ColumnHeaderCell>Full name</Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell>Email</Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell>Status</Table.ColumnHeaderCell>
+            </Table.Row>
+          </Table.Header>
+
+          <Table.Body>
+            {data?.map((s, i) => (
+              <Table.Row key={i}>
                 <Table.RowHeaderCell>{s.fullName}</Table.RowHeaderCell>
                 <Table.Cell>{s.email}</Table.Cell>
-                <Table.Cell>{s.status ? 'Active': 'Inactive'}</Table.Cell>
-                </Table.Row>
-                )}
-
-            </Table.Body>
-          </Table.Root>
+                <Table.Cell>{s.status ? "Active" : "Inactive"}</Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table.Root>
       ) : (
         <div>Loading</div>
       )}
